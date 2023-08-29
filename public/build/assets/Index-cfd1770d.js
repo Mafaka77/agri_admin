1af36decb71d0e4632ea900fca8b06ef8ee83130
Lalfak Zuala <zuala4@gmail.com>
2023-08-29 11:31:50 +0530

    asdasd

--- public/build/assets/Index-cfd1770d.js
+++
@@ -1,1 +0,0 @@
-import{ad as e}from"./app-4d64c5bc.js";const n={name:"Index"};function t(o,r,s,a,c,_){return" Hello "}const p=e(n,[["render",t]]);export{p as default};
---
+++ public/build/assets/Index-f4d94206.js
@@ -0,0 +1,1 @@
+import{ah as e}from"./app-34262ca5.js";const n={name:"Index"};function t(o,r,s,a,c,_){return" Hello "}const d=e(n,[["render",t]]);export{d as default};
